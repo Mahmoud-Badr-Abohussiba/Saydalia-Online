c47bbabc41f3836856c18360417896a55924dc38
Heba Adel Ahmed <HebaAdelAhmedMohammed@gmail.com>
2024-08-24 19:04:29 +0300

    change attribute of image to be imageName

--- Saydalia-Online/Models/Medicine.cs
+++ Saydalia-Online/Models/Medicine.cs
@@ -6,7 +6,7 @@ namespace Saydalia_Online.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string Image { get; set; }
+		public string ImageName { get; set; }
 		public string Description { get; set; }
 		public int Price { get; set; }
 		public int	Stock { get; set; }
