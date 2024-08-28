e8f4acc919c14ba4f5b2ceaa386094ddd5aab223
Mahmoud Badr <mahmoud.hussiba@gmail>
2024-08-29 00:55:51 +0300

    handle-update-medicine

--- Saydalia-Online/Controllers/MedicineController.cs
+++ Saydalia-Online/Controllers/MedicineController.cs
@@ -18,25 +18,27 @@ namespace Saydalia_Online.Controllers
             return View();
         }
 
-        public IActionResult Details() { 
+        public IActionResult Details()
+        {
             return View();
         }
 
         [HttpGet]
-        public IActionResult Create() 
+        public IActionResult Create()
         {
             ViewBag.Categories = _dbContext.categories.ToList();
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(MedicineViewModel medicinVM)
+        public IActionResult Create(MedicineViewModel medicinVM)
         {
             ViewBag.Categories = _dbContext.categories.ToList();
 
-            if (ModelState.IsValid) {
-                medicinVM.ImageName = await DocumentSettings.UploadFile(medicinVM.Image, "images");
-                var NewMedicine = new Medicine() 
+            if (ModelState.IsValid)
+            {
+                medicinVM.ImageName = DocumentSettings.UploadFile(medicinVM.Image, "images");
+                var NewMedicine = new Medicine()
                 {
                     Name = medicinVM.Name,
                     Description = medicinVM.Description,
@@ -46,16 +48,20 @@ namespace Saydalia_Online.Controllers
                     Cat_Id = medicinVM.Cat_Id
                 };
                 _dbContext.Medicines.Add(NewMedicine);
-                _dbContext.SaveChanges();
+                _dbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(medicinVM);
         }
 
         [HttpGet]
-        public IActionResult Edit(int ? id)
+        public IActionResult Edit(int? id)
         {
             var medicine = _dbContext.Medicines.FirstOrDefault(x => x.Id == id);
+            if (medicine == null)
+            {
+                return BadRequest();
+            }
             var model = new MedicineViewModel()
             {
                 Name = medicine.Name,
@@ -70,7 +76,7 @@ namespace Saydalia_Online.Controllers
             return View(model);
         }
         [HttpPost]
-        public async Task<IActionResult> Edit([FromRoute] int id, MedicineViewModel modelVM)
+        public async Task<IActionResult> Edit([FromRoute] int id, MedicineViewModel modelVM, IFormFile Image)
         {
             ViewBag.Categories = _dbContext.categories.ToList();
 
@@ -88,33 +94,28 @@ namespace Saydalia_Online.Controllers
                     if (oldMedicine == null)
                         return BadRequest();
 
-                    if(oldMedicine.ImageName != null)
+                    if (oldMedicine.ImageName != null)
                     {
                         string oldImageName = oldMedicine.ImageName;
                         DocumentSettings.DeleteFile(oldImageName, "images");
                     }
+                    modelVM.ImageName = await DocumentSettings.UploadFile(Image, "images");
+
+                    oldMedicine.Name = modelVM.Name;
+                    oldMedicine.Description = modelVM.Description;
+                    oldMedicine.ImageName = modelVM.ImageName;
+                    oldMedicine.Price = modelVM.Price;
+                    oldMedicine.Stock = modelVM.Stock;
+                    oldMedicine.Cat_Id = modelVM.Cat_Id;
+                    oldMedicine.UpdatedAt = DateTime.Now;
 
 
-                    var UpdatedMedicine = new Medicine()
-                    {
-                        CreatedAt = modelVM.CreatedAt,
-                        Name = modelVM.Name,
-                        Description = modelVM.Description,
-                        ImageName = modelVM.ImageName,
-                        Price = modelVM.Price,
-                        Stock = modelVM.Stock,
-                        Cat_Id = modelVM.Cat_Id,
-                        UpdatedAt = DateTime.Now,
-                        Id = modelVM.Id
-                    };
-                    
-                    var result = _dbContext.Update(UpdatedMedicine);
-                    await _dbContext.SaveChangesAsync();
+                    _dbContext.SaveChanges();
                     return RedirectToAction(nameof(Index));
                 }
                 catch (Exception ex)
                 {
-                    ModelState.AddModelError(string.Empty, ex.Message); 
+                    ModelState.AddModelError(string.Empty, ex.Message);
                 }
             }
             return View(modelVM);
