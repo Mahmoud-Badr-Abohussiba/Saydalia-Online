8d64e68f8d644c595590955d6bd54a5fb17c24f8
Heba Adel Ahmed <HebaAdelAhmedMohammed@gmail.com>
2024-09-02 00:16:58 +0300

    AddOrderModel-and-OrderItemModel

---
+++ Saydalia-Online/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Saydalia_Online.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }     
+        public decimal TotalAmount { get; set; }        
+        public string Status { get; set; }
+
+        // Navigation Properties
+        [ForeignKey("User")]
+        public string UserID { get; set; }
+        public IdentityUser User { get; set; }                  
+        public ICollection<OrderItem> OrderItems { get; set; } 
+    }
+
+}
---
+++ Saydalia-Online/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Saydalia_Online.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemID { get; set; }
+        public int Quantity { get; set; } 
+        public decimal Price { get; set; }
+        [ForeignKey("Order")]
+        public int OrderID { get; set; }
+        // Navigation Properties
+        public Order ? Order { get; set; }
+        [ForeignKey("Medicine")]
+        public int MedicineID { get; set; }
+        public Medicine Medicine { get; set; } 
+    }
+
+}
